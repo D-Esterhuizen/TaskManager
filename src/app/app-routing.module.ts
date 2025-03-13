cf8295526a05941c02bb5471db2c06a8c216d12c
Dylan Esterhuizen <u23668475@tuks.co.za>
2025-03-13 10:05:53 +0200

    added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:"dashboard", component:DashBoardComponent},
+  {path:"about", component:AboutComponent},
+  {path:"", redirectTo: "dashboard", pathMatch:"full"}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
