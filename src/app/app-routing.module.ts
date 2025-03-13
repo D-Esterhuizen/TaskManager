ef209d47cac248080b4e145a8a3b2198b7358a18
Dylan Esterhuizen <u23668475@tuks.co.za>
2025-03-13 10:37:20 +0200

    Created the dashboard with items

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashBoardComponent } from './dash-board/dash-board.component';
+import { DashboardComponent } from './dash-board/dash-board.component';
 import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
 
-  {path:"dashboard", component:DashBoardComponent},
-  {path:"about", component:AboutComponent},
-  {path:"", redirectTo: "dashboard", pathMatch:"full"}
+  {path:"dashboard", component:DashboardComponent},//creatign the dashboard routing 
+  {path:"about", component:AboutComponent},//creatin the routing for the about
+  {path:"", redirectTo: "dashboard", pathMatch:"full"}//makes this the landing page 
 
 ];
 
--- src/app/dash-board/dash-board.component.ts
+++ src/app/dash-board/dash-board.component.ts
@@ -1,11 +1,56 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-dash-board',
+  selector: 'app-dashboard',
   standalone: false,
   templateUrl: './dash-board.component.html',
-  styleUrl: './dash-board.component.css'
+  styleUrl: './dash-board.component.scss'
 })
-export class DashBoardComponent {
+export class DashboardComponent
+{
+  Designation: string = "";
+  Username: string = "";
+  NoOfTeamMembers: number = 0;
+  TotalCostOfAllProjects: number = 0;
+  PendingTasks: number = 0;
+  UpComingProjects: number = 0;
+  ProjectCost: number = 0;
+  CurrentExpenditure: number = 0;
+  AvailableFunds: number = 0;
+  ToDay: Date = new Date();
+
+  Clients: string[] = [];
+  Projects: string[] = [];
+  Years: number[] = [];
+  TeamMembersSummary: any = [];
+  TeamMembers: any = [];
+
+  ngOnInit()
+  {
+    this.Designation = 'Team Leader';
+    this.Username = 'Njabulo Ndlovu';
+    this.NoOfTeamMembers = 15;
+    this.TotalCostOfAllProjects = 240;
+    this.PendingTasks = 25;
+    this.UpComingProjects = 0.5;
+    this.ProjectCost = 2113507;
+    this.CurrentExpenditure = 39225;
+    this.AvailableFunds = 62985;
+    
+
+    this.Clients = [
+      'Poontang Patrol',
+      'Jerkmate ',
+      'Gooners Pty Ltd',
+    ];
+
+   
+
+    for (var i = 2025; i >= 2015; i--)
+    {
+      this.Years.push(i);
+    }
+  }
+
 
 }
